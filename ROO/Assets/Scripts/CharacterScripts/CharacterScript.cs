77a38eb5468c4cad6728ac54ebb7330236c2252f
Liwady <liwadyv@gmail.com>
2022-12-19 19:07:12 +0100

    removed quicksand and added setters for weight gravity and size
    have to make the size scalable in % might give possible array input if I ever have time to polish.

--- ROO/Assets/Scripts/CharacterScripts/CharacterScript.cs
+++ ROO/Assets/Scripts/CharacterScripts/CharacterScript.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
@@ -10,31 +9,151 @@ public class CharacterScript : MonoBehaviour
     private PlayerControls playerControls;
     private GameManager gameManager;
     private CheckerScript checker;
-    private bool dead, canPressButton, canMoveObject;
+    private bool dead, canGrab, canMoveObject;
+    int size;
+    int weight;
+    bool gravity;
     WaterState statusW;
-    QuickSandState statusQ;
 
     private enum WaterState
-    { 
-        CanWalk,
-        Sink,
-        Die
-    }
-    private enum QuickSandState
     {
         CanWalk,
         Sink,
-        Stuck,
         Die
     }
-   
+
+    private void SetSize()
+    {
+        if (checker.checkIfColliderEmpty())//todo checker for mini and make size table 
+        {
+            if (gameManager.abilityActive)
+            {
+                gameManager.character1.transform.localScale = new Vector3(0.9f, 0.4f, 1);
+                gameManager.character2.transform.localScale = new Vector3(0.8f, 1.8f, 1);
+                gameManager.character2.transform.position = new Vector3(gameManager.character2.transform.position.x, gameManager.character2.transform.position.y + 2, gameManager.character2.transform.position.z);
+                gameManager.abilityActive = false;
+            }
+            else
+            {
+                if (gameManager.currentChar == gameManager.character1)
+                {
+                    gameManager.character1.transform.localScale = gameManager.character1.transform.localScale * 2;
+                    gameManager.character2.transform.localScale = gameManager.character2.transform.localScale / 2;
+                }
+                else
+                {
+                    gameManager.character2.transform.localScale = gameManager.character2.transform.localScale * 2;
+                    gameManager.character1.transform.localScale = gameManager.character1.transform.localScale / 2;
+                    gameManager.character2.transform.position = new Vector3(gameManager.character2.transform.position.x, gameManager.character2.transform.position.y + 3, gameManager.character2.transform.position.z);
+                }
+                gameManager.abilityActive = true;
+            }
+        }
+    }
+    private void SetWeight()
+    {
+        if (gameManager.abilityActive)
+        {
+            if (gameManager.currentChar == gameManager.character1) //char1
+            {
+                if (gameManager.currentAbility == 1)//size change
+                {
+                    gameManager.character1.GetComponent<CharacterScript>().weight = 5;
+                    gameManager.character2.GetComponent<CharacterScript>().weight = 1;
+                }
+                else
+                {
+                    gameManager.character1.GetComponent<CharacterScript>().weight = 6;
+                    gameManager.character2.GetComponent<CharacterScript>().weight = 0;
+                }
+            }
+            else //char2
+            {
+                if (gameManager.currentAbility == 1)//size change
+                {
+                    gameManager.character2.GetComponent<CharacterScript>().weight = 4;
+                    gameManager.character1.GetComponent<CharacterScript>().weight = 2;
+                }
+                else
+                {
+                    gameManager.character2.GetComponent<CharacterScript>().weight = 6;
+                    gameManager.character1.GetComponent<CharacterScript>().weight = 0;
+                }
+            }
+        }
+        else
+        {
+            gameManager.character1.GetComponent<CharacterScript>().weight = 4;
+            gameManager.character2.GetComponent<CharacterScript>().weight = 2;
+        }
+    }
+    private void SetGravity()
+    {
+        if (gameManager.currentAbility != 1)
+        {
+            if (gameManager.currentChar == gameManager.character1)
+            {
+                gameManager.character1.GetComponent<CharacterScript>().gravity = true;
+                gameManager.character2.GetComponent<CharacterScript>().gravity = false;
+            }
+            else
+            {
+                gameManager.character1.GetComponent<CharacterScript>().gravity = false;
+                gameManager.character2.GetComponent<CharacterScript>().gravity = true;
+            }
+        }
+        else
+        {
+            gameManager.character1.GetComponent<CharacterScript>().gravity = true;
+            gameManager.character2.GetComponent<CharacterScript>().gravity = true;
+        }
+
+    }
+    private void SetMovementSpeed()
+    {
+        if (gameManager.abilityActive)
+        {
+            if (gameManager.currentChar == gameManager.character1) //char1
+            {
+                if (gameManager.currentAbility == 1)//size change
+                {
+                    movementSpeed = 3;
+                    gameManager.character2.GetComponent<CharacterScript>().movementSpeed = 7;
+                }
+                else
+                {
+                    movementSpeed = 3;
+                    gameManager.character2.GetComponent<CharacterScript>().movementSpeed = 0;
+                }
+            }
+            else //char2
+            {
+                if (gameManager.currentAbility == 1)//size change
+                {
+                    movementSpeed = 4;
+                    gameManager.character1.GetComponent<CharacterScript>().movementSpeed = 6;
+                }
+                else
+                {
+                    movementSpeed = 2;
+                    gameManager.character1.GetComponent<CharacterScript>().movementSpeed = 0;
+                }
+            }
+        }
+        else
+        {
+            gameManager.character1.GetComponent<CharacterScript>().movementSpeed = 5;
+            gameManager.character2.GetComponent<CharacterScript>().movementSpeed = 5;
+        }
+    }
+
     void Awake()
     {
         statusW = WaterState.CanWalk;
-        statusQ = QuickSandState.CanWalk;
         canResize = true;
         checker = GetComponentInChildren<CheckerScript>();
         gameManager = FindObjectOfType<GameManager>();
+        SetWeight();
         playerControls = new PlayerControls();
         playerControls.Gameplay.Move.performed += ctx => movement = ctx.ReadValue<Vector2>(); //lamda expression to preform function
         playerControls.Gameplay.SwitchCharacter.performed += ctx => gameManager.SwitchCharacter();
@@ -62,42 +181,17 @@ public class CharacterScript : MonoBehaviour
         {
             //ability 1, gravity 
             case 0:
+                SetGravity();
                 break;
             //ability 2: size
             case 1:
-
                 if (canResize)
-                {
-                    if (gameManager.abilityActive)
-                    {
-                        gameManager.character1.transform.localScale = new Vector3(0.9f, 0.4f, 1);
-                        gameManager.character2.transform.localScale = new Vector3(0.8f, 1.8f, 1);
-                        gameManager.character2.transform.position = new Vector3(gameManager.character2.transform.position.x, gameManager.character2.transform.position.y + 2, gameManager.character2.transform.position.z);
-
-                        gameManager.abilityActive = false;
-                    }
-                    else if (checker.checkIfColliderEmpty())
-                    {
-                        if (gameObject == gameManager.character1)
-                        {
-                            gameManager.character1.transform.localScale = gameManager.character1.transform.localScale * 2;
-                            gameManager.character2.transform.localScale = gameManager.character2.transform.localScale / 2;
-                        }
-                        else
-                        {
-                            gameManager.character2.transform.localScale = gameManager.character2.transform.localScale * 2;
-                            gameManager.character1.transform.localScale = gameManager.character1.transform.localScale / 2;
-                            gameManager.character2.transform.position = new Vector3(gameManager.character2.transform.position.x, gameManager.character2.transform.position.y + 3, gameManager.character2.transform.position.z);
-                        }
-                        gameManager.abilityActive = true;
-                    }
-                }
-
-                break;
-            //ability 3: combine
-            case 2:
+                    SetSize();
+                //else feedback that u cant 
                 break;
         }
+        SetWeight();
+        SetMovementSpeed();
     }
     private void Floating()
     {
--- ROO/Assets/Scripts/GameManager.cs
+++ ROO/Assets/Scripts/GameManager.cs
@@ -8,30 +8,30 @@ public class GameManager : MonoBehaviour
     public TMP_Text abilityText;
     public GameObject character1;
     public GameObject character2;
-    private int currentChar;
+    public GameObject currentChar;
     public int currentAbility;
     public bool abilityActive;
     private void Awake()
     {
         amountOfFruit = 0;
-        currentChar = 0;
+        currentChar = character1;
         abilityActive = false;
         currentAbility = 1;
         abilityText.text = currentAbility.ToString();
     }
     public void SwitchCharacter()
     {
-        if (currentChar == 0)
+        if (currentChar == character1)
         {
             character1.GetComponent<CharacterScript>().enabled = false;
             character2.GetComponent<CharacterScript>().enabled = true;
-            currentChar = 1;
+            currentChar = character2;
         }
         else
         {
             character2.GetComponent<CharacterScript>().enabled = false;
             character1.GetComponent<CharacterScript>().enabled = true;
-            currentChar = 0;
+            currentChar = character1;
         }
     }
 }
