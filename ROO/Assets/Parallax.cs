98ae7e08f58e36cc2bb5cb68d7cafa5650be1931
Liwady <liwadyv@gmail.com>
2023-01-08 23:52:45 +0100

    level

--- ROO/Assets/Parallax.cs
+++ ROO/Assets/Parallax.cs
@@ -15,8 +15,8 @@ public class Parallax : MonoBehaviour
     private void LateUpdate()
     {
         Vector3 delMovement = cTransform.position - lastCameraPosition;
-        transform.position += delMovement*parallaxEffect;
+        transform.position -= delMovement*parallaxEffect;
         lastCameraPosition = cTransform.position;
-        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -2);
     }
 }
