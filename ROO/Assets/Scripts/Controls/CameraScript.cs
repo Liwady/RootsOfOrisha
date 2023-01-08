3b39b78cf59e0a0e828d60e4191cfc9e639a1706
Liwady <liwadyv@gmail.com>
2023-01-08 14:33:06 +0100

    camera

--- ROO/Assets/Scripts/Controls/CameraScript.cs
+++ ROO/Assets/Scripts/Controls/CameraScript.cs
@@ -5,9 +5,39 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public GameObject player;        //Public variable to store a reference to the player game object
+    [SerializeField]
+    private float upBound;
+    [SerializeField]
+    private float downBound;
+    [SerializeField]
+    private float leftBound;
+    [SerializeField]
+    private float rightBound;
+
+    /*[SerializeField]
+    private float offsettUpDown;
+    [SerializeField]
+    private float offsettLeftRight; */
 
     void Update()
     {
         transform.position = player.transform.position + new Vector3(0, 3, -5);
+
+        transform.position = new Vector3 //keep camera in boundary
+        (
+            Mathf.Clamp(transform.position.x, leftBound, rightBound),
+            Mathf.Clamp(transform.position.y, downBound, upBound),
+            transform.position.z
+        );
+    }
+
+    private void OnDrawGizmos()
+    {
+        //draw a box around camera boundry
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector2(leftBound, upBound), new Vector3(rightBound, upBound));
+        Gizmos.DrawLine(new Vector2(rightBound, upBound), new Vector3(rightBound, downBound));
+        Gizmos.DrawLine(new Vector2(rightBound, downBound), new Vector3(leftBound, downBound));
+        Gizmos.DrawLine(new Vector2(leftBound, downBound), new Vector3(leftBound, upBound));
     }
 }
