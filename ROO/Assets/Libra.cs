7edd2e0c46d9ece98f3d4ac8dbe84c1c90591292
Liwady <liwadyv@gmail.com>
2023-01-09 15:59:59 +0100

    libra arm rotation

--- ROO/Assets/Libra.cs
+++ ROO/Assets/Libra.cs
@@ -4,28 +4,41 @@ using UnityEngine;
 
 public class Libra : MonoBehaviour
 {
-    private LineRenderer lineRenderer;
+    /*private LineRenderer lineRenderer;
     [SerializeField]
-    private Transform[] points;
-    
+    private Transform[] points*/
+    [SerializeField]
+    private Transform[] drawArmPoints;
+
+    [SerializeField]
+    private GameObject arm;
 
     void Start()
     {
-        lineRenderer = GetComponent<LineRenderer>();
-        SetUpLine();
+        //lineRenderer = GetComponent<LineRenderer>();
+        //SetUpLine();
+        SetUpArm();
     }
 
-    
-    void SetUpLine()
+
+
+    /*void SetUpLine()
     {
         lineRenderer.positionCount = points.Length;
+    }*/
+
+    void SetUpArm()
+    {
+        arm.transform.position = new Vector3((drawArmPoints[0].position.x + drawArmPoints[1].position.x) / 2, (drawArmPoints[0].position.y + drawArmPoints[1].position.y) / 2, this.transform.position.z);
+        arm.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(drawArmPoints[1].position.y - drawArmPoints[0].position.y, drawArmPoints[1].position.x - drawArmPoints[0].position.x) * Mathf.Rad2Deg);
     }
 
     private void Update()
     {
-        for (int i = 0; i < points.Length; i++)
+        /*for (int i = 0; i < points.Length; i++)
         {
             lineRenderer.SetPosition(i, points[i].position);
-        }
+        }*/
+        SetUpArm();
     }
 }
