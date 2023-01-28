36c75ffb7df8d3cf8c2bf70a57453c7c4f2d27d3
Liwady <liwadyv@gmail.com>
2023-01-28 01:19:30 +0100

    Update PressurePlate.cs

--- ROO/Assets/Scripts/GameElements/PressurePlate.cs
+++ ROO/Assets/Scripts/GameElements/PressurePlate.cs
@@ -9,6 +9,7 @@ public class PressurePlate : MonoBehaviour
     private bool active, moved;
     public GameObject triggeredChar;
 
+
     private void Update()
     {
         for (int i = 0; i < triggeredObjects.Length; i++)
@@ -16,6 +17,15 @@ public class PressurePlate : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other) //checks if something steps on the pressure plate
     {
+        if (other.CompareTag("Statue"))
+        {
+            active = true;
+            for (int i = 0; i < triggeredObjects.Length; i++)
+            {
+                triggeredObjects[i].GetComponent<TriggerAble>().triggeredChar = other.gameObject;
+                triggeredObjects[i].Toggle(true);
+            }
+        }
         moved = false;
         if (other.CompareTag("1") || other.CompareTag("2"))
         {
@@ -33,6 +43,10 @@ public class PressurePlate : MonoBehaviour
     }
     private void OnTriggerStay(Collider other) //checks if something is on the pressure plate
     {
+        if (other.CompareTag("Statue"))
+        {
+            return;
+        }
         if (other.CompareTag("1") || other.CompareTag("2"))
         {
             CharacterScript character = other.GetComponentInParent<CharacterScript>();
@@ -71,7 +85,7 @@ public class PressurePlate : MonoBehaviour
     private void OnTriggerExit(Collider other) //checks if something leaves the pressure plate
     {
         moved = true;
-        if ((other.CompareTag("1") || other.CompareTag("2")) && triggeredChar == other.gameObject)
+        if ((other.CompareTag("1") || other.CompareTag("Statue") || other.CompareTag("2")) && triggeredChar == other.gameObject)
         {
             active = false;
             for (int i = 0; i < triggeredObjects.Length; i++)
