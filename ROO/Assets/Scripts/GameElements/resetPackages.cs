464f2805701144cb02b718844af6ebfd11897b1d
Liwady <liwadyv@gmail.com>
2023-01-31 00:24:57 +0100

    Level Tracking

--- ROO/Assets/Scripts/GameElements/resetPackages.cs
+++ ROO/Assets/Scripts/GameElements/resetPackages.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class resetPackages : MonoBehaviour
 {
     public GameObject[] packages;
-    private Transform[] packagesOriginalPos;
+    private Vector3[] packagesOriginalPos;
     private void Awake()
     {
-        packagesOriginalPos = new Transform[packages.Length];
+        packagesOriginalPos = new Vector3[packages.Length];
         for (int i = 0; i < packages.Length; i++)
-            packagesOriginalPos[i] = packages[i].transform;
+            packagesOriginalPos[i] = packages[i].transform.position;
     }
     private void OnTriggerEnter(Collider other)
     {
         for (int i = 0; i < packages.Length; i++)
-            packages[i].transform.position = packagesOriginalPos[i].transform.position;
+            packages[i].transform.position = packagesOriginalPos[i];
     }
 }
