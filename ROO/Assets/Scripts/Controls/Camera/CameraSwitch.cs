562aa769ac0bf7e49085f54a4edf7c581f9dbb57
Liwady <liwadyv@gmail.com>
2023-02-11 00:28:07 +0100

    camera script comments

--- ROO/Assets/Scripts/Controls/Camera/CameraSwitch.cs
+++ ROO/Assets/Scripts/Controls/Camera/CameraSwitch.cs
@@ -4,26 +4,26 @@ using Cinemachine;
 public class CameraSwitch : MonoBehaviour
 {
     [SerializeField]
-    private CinemachineVirtualCamera newKoubooCam, newZoubooCam;
-    private bool triggered;
-    private PlayerManager playerManager;
+    private CinemachineVirtualCamera newKoubooCam, newZoubooCam; // two Cinemachine virtual cameras for two characters
+    private bool triggered; // a flag to check if the trigger has already been activated
+    private PlayerManager playerManager; // reference to the PlayerManager component
 
     private void Awake()
     {
-        playerManager = FindObjectOfType<PlayerManager>();
+        playerManager = FindObjectOfType<PlayerManager>(); // find the PlayerManager component in the scene
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (!triggered)
+        if (!triggered) // check if the trigger has not already been activated
         {
-            playerManager.zoubooCam.Priority = -10;
+            playerManager.zoubooCam.Priority = -10; // lower the priority of the current cameras
             playerManager.koobouCam.Priority = -10;
-            playerManager.zoubooCam = newZoubooCam;
+            playerManager.zoubooCam = newZoubooCam; // assign the new cameras to the PlayerManager component
             playerManager.koobouCam = newKoubooCam;
-            if (other.GetComponentInParent<CharacterScript>().Equals(playerManager.character1script))
+            if (other.GetComponentInParent<CharacterScript>().Equals(playerManager.character1script)) // check which character triggered the exit event
             {
-                playerManager.zoubooCam.Priority = 1;
+                playerManager.zoubooCam.Priority = 1; // set the priority of the new cameras for the corresponding characters
                 playerManager.koobouCam.Priority = 0;
             }
             else
@@ -32,7 +32,6 @@ public class CameraSwitch : MonoBehaviour
                 playerManager.koobouCam.Priority = 1;
             }
         }
-
-
     }
 }
+
--- ROO/Assets/Scripts/Controls/Camera/GateCMActivate.cs
+++ ROO/Assets/Scripts/Controls/Camera/GateCMActivate.cs
@@ -1,48 +1,50 @@
+// Gate Camera Activation Script
 using Cinemachine;
 using UnityEngine;
+
 public class GateCMActivate : MonoBehaviour
 {
     [SerializeField]
-    private CinemachineVirtualCamera gateCam;
-    public bool activated;
+    private CinemachineVirtualCamera gateCam; // the virtual camera to activate
+    public bool activated; // boolean to check if the camera is activated
     PlayerManager playerManager;
 
     [SerializeField]
-    private float duration;
-    private float timer;
+    private float duration; // the duration of the cutscene
+    private float timer; // timer for the cutscene
 
     [HideInInspector]
-    private bool cutSceneActive;
+    private bool cutSceneActive; // boolean to check if the cutscene is active
 
     void Start()
     {
-        playerManager = FindObjectOfType<PlayerManager>();
+        playerManager = FindObjectOfType<PlayerManager>(); // finds the PlayerManager
         timer = duration;
-        gateCam.Priority = -5;
+        gateCam.Priority = -5; // deactivates the virtual camera
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (cutSceneActive && !activated)
+        if (cutSceneActive && !activated) // if the cutscene is active and not activated
         {
-            if (timer < 0)
+            if (timer < 0) // if the timer is up
             {
                 playerManager.cutscenePlaying = false;
                 gateCam.Priority = -5;
-                activated = true;
+                activated = true; // activate the camera
             }
-            else if (cutSceneActive)
+            else if (cutSceneActive) // if the cutscene is still active
             {
                 playerManager.cutscenePlaying = true;
-                gateCam.Priority = 3;
-                timer -= Time.deltaTime;
+                gateCam.Priority = 3; // activates the virtual camera
+                timer -= Time.deltaTime; // reduces the timer
             }
         }
 
     }
 
-    public void activatedCutscene()
+    public void activatedCutscene() // activates the cutscene
     {
         cutSceneActive = true;
     }
