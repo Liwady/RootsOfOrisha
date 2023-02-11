62aa378a78a447bd29ef157ea8c40b8277938e08
Liwady <liwadyv@gmail.com>
2023-02-11 01:02:06 +0100

    comments for managrs

--- ROO/Assets/Scripts/Managers/AudioManager.cs
+++ ROO/Assets/Scripts/Managers/AudioManager.cs
@@ -12,54 +12,66 @@ public class AudioManager : MonoBehaviour
     private AudioSource[] wheelAS;
 
 
+    //A public method that plays a sound based on the given input string
 
     public void PlaySound(string _name)
     {
         switch (_name)
         {
-            case "walk1": //char 1
+            //If the input string is "walk1", play the walking sound for character 1
+            case "walk1":
                 if (!player1AS.isPlaying)
                 {
                     player1AS.clip = walkingClipsCharacter1[Random.Range(0, walkingClipsCharacter1.Length)];
                     player1AS.Play();
                 }
                 break;
-            case "walk2": //char 2
+            //If the input string is "walk2", play the walking sound for character 2
+            case "walk2":
                 if (!player2AS.isPlaying)
                 {
                     player2AS.clip = walkingClipsCharacter1[Random.Range(0, walkingClipsCharacter1.Length)];
                     player2AS.Play();
                 }
                 break;
+            //If the input string is "cantResize", play the "cant resize" sound effect
             case "cantResize":
                 cantResizeAS.Play();
                 break;
+            //If the input string is "collect", play the "collect" sound effect
             case "collect":
                 if (!collectAS.isPlaying)
                     collectAS.Play();
                 break;
+            //If the input string is "floatin", play the "float in" sound effect
             case "floatin":
                 if (!floatInAS.isPlaying)
                     floatInAS.Play();
                 break;
+            //If the input string is "floatout", play the "float out" sound effect
             case "floatout":
                 if (!floatOutAS.isPlaying)
                     floatOutAS.Play();
                 break;
+            //If the input string is "sizein", play the "size in" sound effect
             case "sizein":
                 sizeInAS.Play();
                 break;
+            //If the input string is "sizeout", play the "size out" sound effect
             case "sizeout":
                 sizeOutAS.Play();
                 break;
+            //If the input string is "gate", play the "gate" sound effect
             case "gate":
                 if (!gateAS.isPlaying)
                     gateAS.Play();
                 break;
+            //If the input string is "flame", play the "flame" sound effect
             case "flame":
                 if (!flameAS.isPlaying)
                     flameAS.Play();
                 break;
+            //If the input string is "lava", play a random "lava" sound effect from the list
             case "lava":
                 if (!lavaAS.isPlaying)
                 {
@@ -67,9 +79,11 @@ public class AudioManager : MonoBehaviour
                     lavaAS.Play();
                 }
                 break;
+            //If the input string is "swap", play the "swap" sound effect
             case "swap":
                 swapAS.Play();
                 break;
+            //If the input string is "eshulaugh", play a random "eshuLaugh" sound effect from the list
             case "eshulaugh":
                 Debug.Log("c");
                 if (!eshuLaughAS.isPlaying)
@@ -83,6 +97,8 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    // This function plays a sound based on a given name and a number. If the sound name is "wheel", 
+    // it selects a random clip from the wheelClips array and plays it on the AudioSource at the given index.
     public void PlaySound(string _name, int _num)
     {
         switch (_name)
@@ -100,6 +116,7 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    // This function stops a sound based on the given name. If the sound name is "gate", it stops the gateAS audio source.
     public void StopSound(string _name)
     {
         switch (_name)
@@ -111,6 +128,9 @@ public class AudioManager : MonoBehaviour
                 break;
         }
     }
+
+    // This function stops a sound based on the given name and number. If the sound name is "wheel", it stops the audio source 
+    // at the given index.
     public void StopSound(string _name, int _num)
     {
         switch (_name)
--- ROO/Assets/Scripts/Managers/SceneManagment.cs
+++ ROO/Assets/Scripts/Managers/SceneManagment.cs
@@ -10,7 +10,11 @@ using UnityEngine.UI;
 public class SceneManagment : MonoBehaviour
 {
     [SerializeField]
-    private GameObject soundon, soundoff, startObject, startScreen, optionsScreen, pauseScreen, settingsObject, controlsObject, creditsObject, continueObject, settingsChild, controlsChild, creditsChild, currentButtonObject, musicObject, sfxObject, brightnessObject, musicSource, SFXSource;
+    private GameObject soundon, soundoff, startObject, startScreen, optionsScreen, pauseScreen,
+        settingsObject, controlsObject, creditsObject, continueObject,
+        settingsChild, controlsChild, creditsChild,
+        currentButtonObject, musicObject, sfxObject, brightnessObject,
+        musicSource, SFXSource;
     [SerializeField]
     private List<Sprite> scroller, activeSlider;
     [SerializeField]
@@ -34,7 +38,7 @@ public class SceneManagment : MonoBehaviour
     private bool atSlider, start;
     public bool startGame, movedSlider, cutscene;
 
-
+    // Awake function is called before the Start function
     private void Awake()
     {
         brightness.TryGetSettings(out exp);
@@ -49,6 +53,8 @@ public class SceneManagment : MonoBehaviour
             LevelTracker.level = SceneManager.GetActiveScene().buildIndex;
         currentScene = SceneManager.GetActiveScene().buildIndex;
     }
+
+    // Start function is called after the Awake function
     private void Start()
     {
         ss = new SpriteState();
@@ -56,22 +62,29 @@ public class SceneManagment : MonoBehaviour
         currentButtonObject = eventSystem.firstSelectedGameObject;
         SetButtons();
     }
+
+    // Update function is called every frame
     private void Update()
     {
         if (atSlider)
             SliderValue();
     }
 
+    // PlayScene function loads the scene with the given scene number
     public void PlayScene(int sceneNumber)
     {
         SceneManager.LoadScene(sceneNumber);
     }
+
+    // Quit function closes the application
     public void Quit()
     {
         Application.Quit();
     }
+    // SwitchSound function switches the sound on/off
     public void SwitchSound()
     {
+        //Toggles the sound on or off based on the current state of the sound on/off buttons
         if (soundon.activeInHierarchy)
         {
             soundon.SetActive(false);
@@ -87,6 +100,7 @@ public class SceneManagment : MonoBehaviour
     //BUTTONS
     private void SetButtons()
     {
+        //Gets the component of type "Button" for each button object
         sfxButton = sfxObject.GetComponent<Button>();
         musicButton = musicObject.GetComponent<Button>();
         brightnessButton = brightnessObject.GetComponent<Button>();
@@ -97,16 +111,20 @@ public class SceneManagment : MonoBehaviour
     }
     public void ClickButton()
     {
+        //Invokes the onClick event of the current button
         currentButton.onClick.Invoke();
     }
     public void GetCurrentButton()
     {
+        //Sets the current button based on the currently selected game object in the event system
         if (!cutscene)
         {
             currentButtonObject = eventSystem.currentSelectedGameObject;
             currentButton = currentButtonObject.GetComponent<Button>();
         }
     }
+
+    // Sets the currently selected button in the event system, which is then stored for future reference
     public void SetCurrentButton(GameObject button)
     {
         eventSystem.SetSelectedGameObject(button);
@@ -114,7 +132,7 @@ public class SceneManagment : MonoBehaviour
         currentButton = currentButtonObject.GetComponent<Button>();
     }
 
-    //NAVIGATION
+    // Unpauses the game if it's already started, otherwise pauses it
     public void Unpause()
     {
         if (startGame)
@@ -125,8 +143,9 @@ public class SceneManagment : MonoBehaviour
         }
         else
             gameManager.Pause();
-
     }
+
+    // Goes to the start screen, optionally resetting the current selected button to the default
     public void GoToStartScreen(bool first)
     {
         startScreen.SetActive(true);
@@ -139,6 +158,8 @@ public class SceneManagment : MonoBehaviour
         }
         currentScreen = 0;
     }
+
+    // Goes to the pause screen, setting the continue button as the current selected button
     public void GoToPauseScreen()
     {
         pauseScreen.SetActive(true);
@@ -147,61 +168,98 @@ public class SceneManagment : MonoBehaviour
         GetCurrentButton();
         currentScreen = 0;
     }
+
+    //Function to go to the options screen
     public void GoToOptionsScreen()
     {
+        //Turn off start screen if game has started
         if (startGame)
             startScreen.SetActive(false);
+        //Turn off pause screen
         pauseScreen.SetActive(false);
+
+        //Turn on options screen
         optionsScreen.SetActive(true);
+
+        //Turn off controls and credits child screens
         controlsChild.SetActive(false);
         creditsChild.SetActive(false);
+
+        //Turn on settings child screen
         settingsChild.SetActive(true);
+
+        //Set controls, credits, and settings buttons to be enabled
         controlsButton.enabled = true;
         creditsButton.enabled = true;
         settingsButton.enabled = true;
+
+        //Set the current selected game object to the settings button
         eventSystem.SetSelectedGameObject(settingsObject);
+
+        //Get the current selected button
         GetCurrentButton();
+
+        //Set the current screen to 1
         currentScreen = 1;
     }
+
+    //Function to go to the settings screen
     private void GoToSettingsScreen()
     {
-        //change sprite with red line under it for settings 
+        //Turn off controls and credits child screens
         controlsChild.SetActive(false);
         creditsChild.SetActive(false);
+        //Turn on settings child screen
         settingsChild.SetActive(true);
+
+        //Set controls, credits, and settings buttons to be disabled
         controlsButton.enabled = false;
         creditsButton.enabled = false;
         settingsButton.enabled = false;
+
+        //Set the current selected game object based on the current slider value
         if (currentSlider == 1)
             eventSystem.SetSelectedGameObject(sfxObject);
         else if (currentSlider == 2)
             eventSystem.SetSelectedGameObject(brightnessObject);
         else
             eventSystem.SetSelectedGameObject(musicObject);
+
+        //Get the current selected button
         GetCurrentButton();
+
+        //Set the current screen to 2
         currentScreen = 2;
     }
+
+    //Function to enable the button children
     public void EnableButtonChildren(int button)
     {
+        // Switch statement to determine which button was pressed
         switch (button)
         {
-            case 0: //settings
-                GoToSettingsScreen();
+            case 0: // Settings button was pressed
+                GoToSettingsScreen(); // Call function to go to settings screen
                 break;
-            case 1://controls
+            case 1: // Controls button was pressed
+                    // Set relevant children active and other children inactive
                 controlsChild.SetActive(true);
                 settingsChild.SetActive(false);
                 creditsChild.SetActive(false);
-                currentScreen = 1;
+                currentScreen = 1; // Set current screen to controls screen
                 break;
-            case 2://credits
+            case 2: // Credits button was pressed
+                    // Set relevant children active and other children inactive
                 controlsChild.SetActive(false);
                 settingsChild.SetActive(false);
                 creditsChild.SetActive(true);
-                currentScreen = 1;
+                currentScreen = 1; // Set current screen to credits screen
                 break;
         }
     }
+
+
+    //Function to go back to the previous screen
     public void GoBack()
     {
         switch (currentScreen)
@@ -226,87 +284,143 @@ public class SceneManagment : MonoBehaviour
             case 4:
                 PlayScene(0);
                 break;
-
         }
     }
 
     //SLIDERS
+    //Function to update the value of the slider
     private void SliderValue()
     {
+        //Check if the slider has been moved and enough time has passed
         if (movedSlider && time > Time.unscaledDeltaTime)
         {
+            //Increase or decrease the old value depending on the direction of the movement
             if (valueS.x > 0)
                 old++;
             else if (valueS.x < 0)
                 old--;
 
+            //Ensure that the value remains within the acceptable range
             if (old <= 0)
                 old = 0;
             else if (old >= 10)
                 old = 10;
 
+            //Update the value of the slider
             slider.value = old;
+
+            //Reset the time and set the movedSlider flag to false
             time = 0;
             movedSlider = false;
         }
         else
+            //Increment the time
             time += Time.unscaledDeltaTime;
 
     }
+
+    // method to handle the button interaction with the slider
     public void SliderButton(int button)
     {
+        // storing the current button number that was interacted with
         currentSlider = button;
+
+        // if the game has not started
         if (!start)
+        {
+            // deactivate the slider
             DeactivateSlider();
+        }
+        // if the game has started
         else
+        {
+            // activate the slider with the current button number
             ActivateSlider(currentSlider);
+        }
     }
+
+
     private void ActivateSlider(int button)
     {
+        // Switch based on the button passed as argument
         switch (button)
         {
-            case 0: //music
+            case 0: // case for music button
+                    // enable music button and disable sfx and brightness buttons
                 musicButton.enabled = true;
                 sfxButton.enabled = false;
                 brightnessButton.enabled = false;
+                // set the selected game object to the music object
                 eventSystem.SetSelectedGameObject(musicObject);
+                // set the sprite state of the selected slider to the active sprite
                 ss.selectedSprite = activeSlider[0];
                 break;
-            case 1://sfx
+            case 1: // case for sfx button
+                    // enable sfx button and disable music and brightness buttons
                 musicButton.enabled = false;
                 sfxButton.enabled = true;
                 brightnessButton.enabled = false;
+                // set the selected game object to the sfx object
                 eventSystem.SetSelectedGameObject(sfxObject);
+                // set the sprite state of the selected slider to the active sprite
                 ss.selectedSprite = activeSlider[1];
                 break;
-            case 2://brightness
+            case 2: // case for brightness button
+                    // enable brightness button and disable music and sfx buttons
                 musicButton.enabled = false;
                 sfxButton.enabled = false;
                 brightnessButton.enabled = true;
+                // set the selected game object to the brightness object
                 eventSystem.SetSelectedGameObject(brightnessObject);
+                // set the sprite state of the selected slider to the active sprite
                 ss.selectedSprite = activeSlider[2];
                 break;
         }
+
+        // Get the current button based on the selected slider
         GetCurrentButton();
+        // set the sprite state of the current button
         currentButton.spriteState = ss;
+        // Get the slider component from the current button
         slider = currentButton.GetComponentInChildren<Slider>();
+        // set the sprite of the slider handle
         slider.GetComponentInChildren<Image>().sprite = scroller[0];
+        // set the sprite of the slider fill
         slider.transform.GetChild(1).gameObject.GetComponentInChildren<Image>().sprite = scroller[2];
+        // enable the slider
         slider.enabled = true;
+        // store the old value of the slider
         old = (int)slider.value;
+        // set the atSlider flag to true
         atSlider = true;
+        // set the start flag to false
         start = false;
+        // set the current screen to 3
         currentScreen = 3;
     }
+
     private void DeactivateSlider()
     {
+        // Set the "atSlider" flag to false
         atSlider = false;
+
+        // Set the "start" flag to true
         start = true;
+
+        // Set the "valueS" vector to (0, 0)
         valueS = Vector2.zero;
+
+        // Disable the slider
         slider.enabled = false;
+
+        // Change the sprite of the slider's track and handle
         slider.GetComponentInChildren<Image>().sprite = scroller[1];
         slider.transform.GetChild(1).gameObject.GetComponentInChildren<Image>().sprite = scroller[3];
+
+        // Reset the "slider" variable to null
         slider = null;
+
+        // Change the sprite of the current button based on its type
         switch (currentSlider)
         {
             case 0: //music
@@ -319,25 +433,47 @@ public class SceneManagment : MonoBehaviour
                 ss.selectedSprite = activeSlider[5];
                 break;
         }
+
+        // Apply the new sprite state to the current button
         currentButton.spriteState = ss;
+
+        // Enable the music, sfx, and brightness buttons
         musicButton.enabled = true;
         sfxButton.enabled = true;
         brightnessButton.enabled = true;
+
+        // Call the "GoBack" function to return to the previous screen
         GoBack();
     }
+
+
+    // This function is used to set the volume of the music based on the value of the slider.
     public void SetMusicVolume()
     {
+        // If the value of the slider is 0, set the volume of the music to 0
         if (slider.value == 0)
+        {
             musicSource.GetComponent<AudioSource>().volume = 0;
+        }
+        // Else, set the volume of the music to (slider value / 10)
         else
+        {
             musicSource.GetComponent<AudioSource>().volume = (slider.value / 10);
+        }
     }
+
+
+    //The function "SetSFXVolume" sets the volume of the sound effects in the game.
     public void SetSFXVolume()
     {
+        //This comment states that the code within this function is a temporary fix and needs to be overhauled.
 
-        //to overhaul later temporary fix
+        //The switch statement checks the value of the slider.
         switch (slider.value)
         {
+            //In each case, the volume of the sound effects is set using the "SFXMasterMixer" object and the "SetFloat" method.
+            //The first argument is the name of the float parameter in the audio mixer.
+            //The second argument is the value to set the parameter to.
             case 0:
                 SFXMasterMixer.SetFloat("MasterVolSFX", -80);
                 break;
@@ -374,18 +510,21 @@ public class SceneManagment : MonoBehaviour
             default:
                 break;
         }
-
     }
-    public void SetBrightness()//5=1.5 brighness 10=2 brightness 0=1
+
+    //The function "SetBrightness" changes the brightness of the scene by adjusting the post-exposure value of the camera's exposure component.
+    public void SetBrightness()
     {
         float value = slider.value;
 
+        //If the value of the slider is 0, the brightness is set to 0/8
         if (slider.value == 0)
             value = 0 / 8f;
+        //If the value of the slider is between 0 and 10, the brightness is set to 0.8 + (slider.value/10)
         else if (slider.value <= 10 && slider.value > 0)
             value = 0.8f + (slider.value / 10);
 
-
+        //Set the post-exposure value to the calculated value
         exp.postExposure.value = value;
     }
 
